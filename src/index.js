20d5935a185a518dcfab8cbdff6288f38807de3c
cherieliu <cherieliu27@gmail.com>
2023-02-18 18:33:17 -0500

    wrote method for finding all rgb colors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,84 @@
 const fs = require('fs');
 const path = require('path');
+// const getColors = require('get-image-colors');
+const getPixels = require("get-pixels");
+const PNG = require('pngjs').PNG;
 
-export function parseImage(imagePath) {
-    console.log('IN HERE');
+export function parseImageToBuffer(imagePath) {
     try {
         console.log(imagePath);
-        const imageBuffer= fs.readFileSync(imagePath);
-        console.log(imageBuffer.shape);
+        const imageBuffer = fs.readFileSync(imagePath);
+        return imageBuffer;
     } catch (err) {
         console.log(err);
     }
+}
+
+export function getRGBColors(imageBuffer) {
+    var colors = [];
+    var pixels, r, g, b, a;
+    r = g = b = a = 0;
+    var i = 0;
+
+    console.log(imageBuffer);
+
+    var png = PNG.sync.read(imageBuffer).data;
+    pixels = png.length / 4;
+
+    for (i = 0; i < pixels; i++) {
+        r = png[i];
+        g = png[i+1];
+        b = png[i+2];
+        a = png[i+3];
+        // pixel is > 50% transparent
+        /*if (a < (255 / 2)) {
+            continue;
+        }*/
+
+        let color = [r, g, b];
+        colors.push(color);
+    }
+
+    return colors;
+
+    // console.log(colors[15000]);
+
+    /*for (const value of png.data.values()) {
+        if (i % 4 == 0) {
+            r = 
+        }
+    }*/
+
+    /*for (const value of imageBuffer) {
+        console.log(value);
+        if (i == 4) {
+            break;
+        }
+        i += 1;
+    }*/
+
+    /*var colors = [];
+    getPixels(imageBuffer, 'image/png', function(err, pixels) {
+        if (err) {clear
+
+            console.log("Bad Image Path");
+            return;
+        }
+
+        console.log(pixels.data);
+        colors.push(1);
+    })*/
+    // var png = PNG.sync.read(imageBuffer);
+    // console.log(png.data);
+
+
+
+    /*getPixels(imageBuffer, 'image/png', function(err, pixels) {
+        if (err) {
+            console.log("Bad Image Path");
+            return;
+        } 
+        imPixels = pixels;
+    })*/
+    return colors;
 }
--- test/spec.js
+++ test/spec.js
@@ -1,14 +1,27 @@
-/*const utils = require('..');
-const assert = require('assert');
-
-utils.parseImage('/Users/cherieliu/Desktop/Spring 2023/OSS/color-themer/test/testimg1.png');
-*/
-
 const utils = require('..');
 const assert = require('assert');
 const path = require('path');
+const sizeOf = require('image-size');
 
 let impath = path.join(__dirname, 'testimg1.png');
-utils.parseImage(impath);
+
+/* Testing Values */
+let imsize = 145756;
+let imdims = [1200, 1200];
+
+/* Tests on testimg1.png */
+let imbuff = utils.parseImageToBuffer(impath);
+assert.equal(imbuff.length, imsize);
+
+let imPixels = [];
+// console.log(imbuff);
+let colors = utils.getRGBColors(imbuff);
+// check number of pixels == number of RGB colors
+assert.equal(colors.length, imdims[0] * imdims[1]);
+// console.log(colors);
+// console.log(imPixels);
+// ssert.equal(impixels.shape, imdims);
+// assert.equal(impixels.shape.slice()[1], imheight);
+
 
 console.log(`\u001B[32m✓\u001B[39m Tests passed`);
